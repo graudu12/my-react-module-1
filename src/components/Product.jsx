418a132ffad8f59ea07318160135708aaf48d386
Dima <graudu_12@yahoo.co.uk>
2025-02-17 12:16:49 +0000

    jsx

---
+++ src/components/Product.jsx
@@ -0,0 +1,30 @@
+// src/Product.jsx
+
+export default function Product()
+{
+  return (
+    <div>
+		  <h2>Tacos</h2>
+		  <img src="https://images.pexels.com/photos/461198/pexels-photo-461198.jpeg?dpr=2&h=480&w=640" alt="Tacos With Lime" width="640" />
+	  <p>Price: 999 credits</p>
+    </div>
+  );
+};
+// const Mailbox = ({ username, messages }) => {
+//   return (
+//     <>
+//       <p>Hello {username}</p>
+//       {messages.length > 0 && (
+//         <p>You have {messages.length} unread messages</p>
+//       )}
+//     </>
+//   );
+// };
+
+// 	<div>
+// 		{[1, 2, 3].map(item => {
+// 			return <div>{item}</div>;
+// 		})}
+// 	</div>
+
+
